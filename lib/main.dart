8510053b4350d4c712058a4224a23b3a545e649b
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-11-15 13:04:31 +0300

    completed code

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.greenAccent,
+      ),
       home: new RandomWords(),
 //      title: 'Welcome to Flutter',
 //      home: new Scaffold(
@@ -23,6 +26,8 @@ class RandomWordsState extends State<RandomWords> {
 
   final List<WordPair> _suggestions=<WordPair>[];
   final TextStyle _biggerFont =const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = new Set<WordPair>();   // Add this line.
+
   @override
   Widget build(BuildContext context) {
 
@@ -33,12 +38,46 @@ class RandomWordsState extends State<RandomWords> {
   return new Scaffold(
     appBar: new AppBar(
       title: new Text("Startup Name Generator"),
+      actions: <Widget>[      // Add 3 lines from here...
+        new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+      ],
     ),
+
     body: _buildSuggestions(),
 
   );
   }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(   // Add 20 lines from here...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
 
+          return new Scaffold(         // Add 6 lines from here...
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );                           // ... to here.
+        },
+      ),                           // ... to here
+    );
+  }
   Widget _buildSuggestions(){
     return new ListView.builder(
       padding: const EdgeInsets.all(16.0),
@@ -75,11 +114,26 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);  // Add this line.
+
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(   // Add the lines from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {      // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
